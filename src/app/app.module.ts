4fd28138abb3a71adc7fa51e54378f06b7c4dfb5
Nahuel Ramos <nahuelramos.nr@gmail.com>
2018-03-14 19:24:09 -0300

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule

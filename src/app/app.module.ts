555d0f55595c9eb9eb6a31fd8c3c52488655743d
Nahuel Ramos <nahuelramos.nr@gmail.com>
2018-03-15 21:43:03 -0300

    add a simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +9,12 @@ import { AccountComponent } from './account/account.component';
 import { AccountTypeComponent } from './account/account-type/account-type.component';
 import { AccountRegisterComponent } from './account/account-register/account-register.component';
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'account-register', component: AccountRegisterComponent },
+  { path: 'account-type', component: AccountTypeComponent }
+]
 
 @NgModule({
   declarations: [
@@ -20,7 +26,8 @@ import { AccountRegisterComponent } from './account/account-register/account-reg
     AccountRegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

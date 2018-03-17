0576befe4cb19fc5b2bc42b02df842d6afa8047e
Nahuel Ramos <nahuelramos.nr@gmail.com>
2018-03-17 12:04:08 -0300

    add unsubscribe in account-type

--- src/app/account/account-type/account-type.component.ts
+++ src/app/account/account-type/account-type.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../shared/account.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import { AccountService } from '../shared/account.service';
 
 @Component({
   selector: 'app-account-type',
@@ -8,16 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./account-type.component.css'],
   providers: [AccountService]
 })
-export class AccountTypeComponent implements OnInit {
+export class AccountTypeComponent implements OnInit, OnDestroy {
   arrayFormType: {};
+  accountServiceObservableGet: Subscription;
 
   constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit() {
-    this.accountService.getAccountTypeForm().subscribe(
+    this.accountServiceObservableGet = this.accountService.getAccountTypeForm().subscribe(
       (serverResponse: any) => { this.arrayFormType = serverResponse.form; },
       (error: any) => { alert(error); }
     );
+  }
+
+  ngOnDestroy() {
+    this.accountServiceObservableGet.unsubscribe();
   }
 
   accountTypeSelected(accountType) {
